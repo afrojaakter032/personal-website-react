8c30b4a1e0b239fea7bae4a1d1fec5d7076ae490
afrojaakter032 <afroja.cu.cse@gmail.com>
2023-11-01 01:03:07 +0600

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <main>
+        <section className='hero'>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-lg-12 hero-image' style={{height:700, background: "url(./assets/images/banner_bg2.png)", backgroundSize:"cover"}}>
+                    
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        </section>
+
+    </main>
+    <footer>
+
+    </footer>
+    
+    
+    </>
+    
+  );
+}
