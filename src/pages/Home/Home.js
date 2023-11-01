47139cce860c5d96a02901de6dc06615509a0d58
afrojaakter032 <afroja.cu.cse@gmail.com>
2023-11-02 01:20:15 +0600

    hero section content add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,42 +3,25 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-    <main>
-        <section className='hero' style={{height:700, background: 'url(./assets/images/banner_bg2.png)', backgroundSize:"cover"}}>
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-lg-12 hero-image'>
-                  <div className='hero-content'>
-                    <div className='hero-item'>
-                        <p className='h1 hero-header'>Hi, I'M ZYAN!</p>
-                        <p className='h1 hero-sub-header'>CREATIVE CODER</p>
+        <main>
+            <section className="hero">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-12 hero-image">
+                            <div className="px-3 pt-5">
+                                <div className="pt-5 fw-bold display-4">
+                                    <div>Hi, I'M AFROZA!</div>
+                                    <div>CREATIVE CODER</div>
+                                </div>
+                                <div className="pt-3">
+                                    <p className="fs-5 w-50">I'm a passionate UI/UX designer with a mission to create delightful and intuitive digital experiences. With a strong foundation in design principles and a keen eye for detail, I specialize in translating complex ideas into user-friendly interfaces that captivate and engage.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div className='hero-item'>
-                      <p>I'm a passionate UI/UX designer with a mission to create delightful and intuitive digital experiences. With a strong foundation in design principles and a keen eye for detail, I specialize in translating complex ideas into user-friendly interfaces that captivate and engage.</p>
-                    </div>
-
-
-                  </div>
-
                 </div>
-
-            </div>
-
-
-        </div>
-
-
-        </section>
-
-
-    </main>
-    <footer>
-
-    </footer>
-
-
+            </section>
+        </main>
     </>
-
   );
 }
