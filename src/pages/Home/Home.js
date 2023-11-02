03a9cd3d8993beceb2b567a3ff51c7a14dbc4e37
Afroja <afroja.cu.cse@gmail.com>
2023-11-02 16:10:55 +0600

    hero section button add

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,6 +16,11 @@ export default function Home() {
                                 <div className="pt-3">
                                     <p className="fs-5 w-50">I'm a passionate UI/UX designer with a mission to create delightful and intuitive digital experiences. With a strong foundation in design principles and a keen eye for detail, I specialize in translating complex ideas into user-friendly interfaces that captivate and engage.</p>
                                 </div>
+                                <div classname="d-flex">
+                                    <button classname="btn">Dawnload Cv <i class="ph ph-download-simple"> </i></button>
+
+                                    <button classname="btn"><i class="ph ph-play"></i> Watch the video </button>
+                                </div>
                             </div>
                         </div>
                     </div>
