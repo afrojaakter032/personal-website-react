c2b0adf50af0852125166f1e89a93f5de3f8e093
afrojaakter032 <afroja.cu.cse@gmail.com>
2023-12-13 00:23:57 +0600

    api add

---
+++ src/pages/BlogDetails.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+function BlogDetails (){
+    return(
+        <>
+            <div className="row">
+                <div className="col-lg-12">
+                    <img src="./assets/images/8.png" alt="image" />
+                    <div className="fs-2">Title</div>
+
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default BlogDetails;
